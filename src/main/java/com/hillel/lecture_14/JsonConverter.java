3aed103fce65c28562aa7cf7b9970517ba2c9541
Tkalenko Mykyta <mykyta.tkalenko@moyo.u>
2020-04-23 14:57:57 +0300

    task lecture 14

--- src/main/java/com/hillel/lecture_14/JsonConverter.java
+++ src/main/java/com/hillel/lecture_14/JsonConverter.java
@@ -1,5 +1,10 @@
 package com.hillel.lecture_14;
 
+import java.lang.reflect.AnnotatedType;
+import java.lang.reflect.Field;
+import java.sql.Array;
+import java.util.Arrays;
+import java.util.*;
 import java.util.List;
 
 /**
@@ -17,10 +22,14 @@ import java.util.List;
 public class JsonConverter {
 
 
-    public String convertToJsonString(List<User> users) {
+    public String convertToJsonString(List<User> users){
 
 //        TODO implements result
         String result = "";
+
+//        ArrayList<HashMap> hashUsers = new ArrayList<HashMap>();
+//        for (User user:users) {
+//        }
 
         return result;
     }
@@ -30,8 +39,25 @@ public class JsonConverter {
 //        TODO implements result
         String result = "";
 
+        HashMap<String, Object> huser = new HashMap<>();
+
+        Field[] declaredFields = users.getClass().getDeclaredFields();
+        for (Field field:declaredFields) {
+            if (!field.isAccessible()) {
+                field.setAccessible(true);
+            }
+            try {
+                huser.put(field.getName(), field.get(users));
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println(huser);
+
+
         return result;
     }
+
 
 
 }
